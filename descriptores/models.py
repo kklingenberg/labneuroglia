298654c7cfdce5b16db85bef7c49d52b8b6fcc94
Kai Klingenberg <k.klingenberg@gmail.com>
2013-06-10 00:01:36 -0400

    Mejoras en descriptores de módulos.

--- descriptores/models.py
+++ descriptores/models.py
@@ -2,23 +2,20 @@
 import re
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-sub_bold = lambda text: re.sub(
-    "\*(.*?)\*",
-    lambda mobj: u"<strong>{0}</strong>".format(mobj.group(1)),
+make_sub = lambda char, regchar, opentag, closetag: lambda text: re.sub(
+    "{0}(.*?){0}".format(regchar),
+    lambda mobj: u"{0}{1}{2}".format(opentag, mobj.group(1), closetag) \
+        if mobj.group(1) else char,
     text)
 
-sub_italic = lambda text: re.sub(
-    "_(.*?)_",
-    lambda mobj: u"<em>{0}</em>".format(mobj.group(1)),
-    text)
 
-sub_titles = lambda text: re.sub(
-    "=(.*?)=",
-    lambda mobj: u'<span style="font-size: 1.5em;">{0}</span>'.\
-        format(mobj.group(1)),
-    text)
+sub_bold = make_sub("*", "\*", u"<strong>", u"</strong>")
+sub_italic = make_sub("_", "_", u"<em>", u"</em>")
+sub_titles = make_sub("=", "=", u'<span style="font-size: 1.5em;">', u"</span>")
 
 
 class Descripcion(models.Model):
@@ -34,19 +31,26 @@ class Descripcion(models.Model):
         choices=target_options,
         help_text=u"Módulo a describir")
 
+    enabled = models.BooleanField(verbose_name=u"activado", default=True)
+
     body = models.TextField(
         verbose_name=u"Descripción del módulo",
         blank=True,
-        help_text=u"Escriba frases entre asteriscos (*) para que se muestren "\
-            u"en negrita. Use guiones bajos (_) para que se muestren "\
-            u"en cursiva. Use el signo igual (=) para títulos.")
+        help_text=u'<span style="font-size: 1.2em;">Escriba frases entre '\
+            u'asteriscos (*) para que se muestren en negrita. '\
+            u'Use guiones bajos (_) para que se muestren en cursiva. '\
+            u'Use el signo igual (=) para títulos.<br />'\
+            u'Si quiere usar alguno de esos símbolos, '\
+            u'insértelo dos veces (e.g. **).</span>')
 
     class Meta:
         verbose_name = u"descripción de módulo"
         verbose_name_plural = u"descripciones de módulos"
 
     def __unicode__(self):
-        return u"Descripción para {0}".format(dict(self.target_options)[self.target])
+        return u"Descripción para {0}{1}".format(
+            dict(self.target_options)[self.target],
+            u"EN USO ACTUALMENTE" if self.enabled else u"")
 
     def body_to_html(self):
         # split in paragraphs
@@ -58,3 +62,9 @@ class Descripcion(models.Model):
         # sub italic
         pars = map(sub_italic, pars)
         return "".join(map(lambda p: u"<p>{0}</p>\n".format(p.strip()), pars))
+
+@receiver(post_save, sender=Descripcion, dispatch_uid="set_rest_disabled")
+def set_rest_disabled(sender, instance, **kwargs):
+    if instance.enabled:
+        Descripcion.objects.filter(
+            ~models.Q(id=instance.id), enabled=True).update(enabled=False)
